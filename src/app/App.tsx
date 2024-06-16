41ad5489cbe66bcd9f45d4a603b05fa2b4d42b4c
Dmytro <dima06444@gmail.com>
2024-06-16 19:59:57 +0300

    refactor: remove unnecessary code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from 'app/providers/theme';
 import { AppRouter } from 'app/router';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Loader } from 'shared/ui/Loader';
 import { NavBar } from 'widgets/NavBar';
@@ -9,12 +9,6 @@ import './styles/index.css';
 
 const App = () => {
   const { theme } = useTheme();
-
-  useEffect(() => {
-    if (Math.random() > 0.5) {
-      throw new Error('Random error');
-    }
-  });
 
   return (
     <div className={classNames('app', {}, [theme])}>
