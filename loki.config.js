1110278dfd5cb7de2e27f3e6fe2c6f62af4ffd2e
Dmytro <dima06444@gmail.com>
2024-07-03 00:40:41 +0300

    refactor: update screenshots

--- loki.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  storiesFilter: (test) => {
-    console.log('test', test)
-  }
-}
--- src/shared/config/storybook/withI18nextDecorator.tsx
+++ src/shared/config/storybook/withI18nextDecorator.tsx
@@ -1,5 +1,4 @@
 import type { StoryFn } from "@storybook/react/*";
-import { Suspense } from "react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "shared/config/i18n/i18n";
 
@@ -7,10 +6,10 @@ export const withI18nextDecorator = (StoryComponent: StoryFn) => {
   return (
     // This catches the suspense from components not yet ready (still loading translations)
     // Alternative: set useSuspense to false on i18next.options.react when initializing i18next
-    <Suspense fallback={<div>loading translations...</div>}>
-      <I18nextProvider i18n={i18n}>
-        <StoryComponent />
-      </I18nextProvider>
-    </Suspense>
+    // <Suspense fallback={<div>loading translations...</div>}>
+    <I18nextProvider i18n={i18n}>
+      <StoryComponent />
+    </I18nextProvider>
+    // </Suspense>
   );
 };
