dbdfdb492c17121c9872e5fd8d3246cbe516d309
Dmytro <dima06444@gmail.com>
2024-07-04 23:13:01 +0300

    test: fix tests

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -5,17 +5,15 @@ import { Sidebar } from './Sidebar';
 describe('Sidebar', () => {
   test('should toggle collapse state on button click', () => {
     const { getByText, container } = renderWithTranslation(<Sidebar />);
-    const button = getByText('Collapse');
+    const button = getByText('>');
     const sidebar = container.querySelector('.sidebar');
 
-    fireEvent.click(button);
-
     expect(sidebar).toHaveClass('collapsed');
-    expect(button).toHaveTextContent('Expand');
+    expect(button).toHaveTextContent('>');
 
     fireEvent.click(button);
 
     expect(sidebar).not.toHaveClass('collapsed');
-    expect(button).toHaveTextContent('Collapse');
+    expect(button).toHaveTextContent('<');
   });
 });
