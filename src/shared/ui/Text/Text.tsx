dedfd5aaa5259842746bf37447540f3454963101
Dmytro <dima06444@gmail.com>
2024-09-04 00:08:57 +0300

    feat: add text align to Text component

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -6,22 +6,32 @@ export enum TextTheme {
   ERROR = "error",
 }
 
+export enum TextAlign {
+  CENTER = "center",
+  LEFT = "left",
+  RIGHT = "right",
+}
+
 interface Props {
   className?: string;
   title?: string;
   text?: string;
   theme?: TextTheme;
+  align?: TextAlign;
 }
 
-export const Text = memo(({ className, text, title, theme }: Props) => {
-  return (
-    <div
-      className={classNames(styles.text, { [styles[theme!]]: !!theme }, [
-        className,
-      ])}
-    >
-      {title && <h2 className={styles.title}>{title}</h2>}
-      {text && <p className={styles.text}>{text}</p>}
-    </div>
-  );
-});
+export const Text = memo(
+  ({ className, text, title, theme, align = TextAlign.LEFT }: Props) => {
+    return (
+      <div
+        className={classNames(styles.text, { [styles[theme!]]: !!theme }, [
+          className,
+          styles[align],
+        ])}
+      >
+        {title && <h2 className={styles.title}>{title}</h2>}
+        {text && <p className={styles.text}>{text}</p>}
+      </div>
+    );
+  },
+);
