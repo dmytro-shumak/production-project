d7663fbd6192365202195b273d2feecc869f03e5
Dmytro <dima06444@gmail.com>
2024-09-20 21:49:49 +0300

    feat: add skeleton to comment list

--- src/entities/Comment/ui/CommentList/CommentList.tsx
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -1,10 +1,9 @@
 import { memo } from "react";
-import { classNames } from "shared/lib/classNames/classNames";
-import { Text } from "shared/ui";
 import { useTranslation } from "react-i18next";
-import { Loader } from "shared/ui/Loader";
-import { CommentItem } from "../CommentItem/CommentItem";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Skeleton, Text } from "shared/ui";
 import type { Comment } from "../../model/types/comment";
+import { CommentItem } from "../CommentItem/CommentItem";
 import styles from "./CommentList.module.css";
 
 interface Props {
@@ -17,7 +16,13 @@ export const CommentList = memo(({ className, comments, isLoading }: Props) => {
   const { t } = useTranslation("");
 
   if (isLoading) {
-    return <Loader className={styles.loader} />;
+    return (
+      <div>
+        <Skeleton className={styles.skeleton} height={86} />
+        <Skeleton className={styles.skeleton} height={86} />
+        <Skeleton className={styles.skeleton} height={86} />
+      </div>
+    );
   }
 
   return (
