830e1881c395decade30d4164ebd1605a4849ff1
Dmytro <dima06444@gmail.com>
2024-09-20 21:57:53 +0300

    feat(profile): add can edit profile selector

---
+++ src/pages/ProfilePage/model/selectors/canEditProfile/canEditProfile.tsx
@@ -0,0 +1,9 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { getProfileData } from "entities/Profile";
+import { getUserAuthData } from "entities/User";
+
+export const canEditProfileSelector = createSelector(
+  getUserAuthData,
+  getProfileData,
+  (authData, profileData) => authData?.id === profileData?.id,
+);
--- src/pages/ProfilePage/ui/ProfilePageHeader/ProfilePageHeader.tsx
+++ src/pages/ProfilePage/ui/ProfilePageHeader/ProfilePageHeader.tsx
@@ -1,16 +1,15 @@
-import { useCallback, type FC } from "react";
-import { classNames } from "shared/lib/classNames/classNames";
-import { Button, ButtonTheme } from "shared/ui";
-import { Text } from "shared/ui/Text/Text";
-import { useTranslation } from "react-i18next";
-import { useAppDispatch, useAppSelector } from "shared/lib";
 import {
-  getProfileData,
   getProfileReadOnly,
   profileActions,
   updateProfileData,
 } from "entities/Profile";
-import { getUserAuthData } from "entities/User";
+import { useCallback, type FC } from "react";
+import { useTranslation } from "react-i18next";
+import { useAppDispatch, useAppSelector } from "shared/lib";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Button, ButtonTheme } from "shared/ui";
+import { Text } from "shared/ui/Text/Text";
+import { canEditProfileSelector } from "../../model/selectors/canEditProfile/canEditProfile";
 import styles from "./ProfilePageHeader.module.css";
 
 interface Props {
@@ -20,9 +19,7 @@ interface Props {
 export const ProfilePageHeader: FC<Props> = ({ className }) => {
   const { t } = useTranslation("profile");
 
-  const authData = useAppSelector(getUserAuthData);
-  const profileData = useAppSelector(getProfileData);
-  const canEdit = authData?.id === profileData?.id;
+  const canEditProfile = useAppSelector(canEditProfileSelector);
 
   const readOnly = useAppSelector(getProfileReadOnly);
   const dispatch = useAppDispatch();
@@ -45,7 +42,7 @@ export const ProfilePageHeader: FC<Props> = ({ className }) => {
   return (
     <div className={classNames(styles.profilePageHeader, {}, [className])}>
       <Text title={t("Profile")} />
-      {canEdit && (
+      {canEditProfile && (
         <>
           <Button
             theme={readOnly ? ButtonTheme.Outline : ButtonTheme.OutlineRed}
