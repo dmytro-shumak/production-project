6fae0e2dc2fada19c6b282ee0ac37acf3b77d653
Dmytro <dima06444@gmail.com>
2024-09-22 14:18:37 +0300

    test: add articleList stories

---
+++ src/entities/Article/ui/ArticleList/ArticleList.stories.tsx
@@ -0,0 +1,136 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Theme } from "app/providers/theme";
+import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator";
+import { ArticleView, type Article } from "../../model/types/article";
+import { ArticleList } from "./ArticleList";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "entities/ArticleList",
+  component: ArticleList,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+} satisfies Meta<typeof ArticleList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const articleItem = {
+  id: "1",
+  title: "Javascript news",
+  subtitle: "Что нового в JS за 2022 год?",
+  img: "https://teknotower.com/wp-content/uploads/2020/11/js.png",
+  views: 1022,
+  createdAt: "26.02.2022",
+  user: {
+    id: "1",
+    username: "Ulbi Tv",
+    avatar:
+      "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&q=70&fm=webp",
+  },
+  type: ["IT"],
+  blocks: [
+    {
+      id: "1",
+      type: "TEXT",
+      title: "Заголовок этого блока",
+      paragraphs: [
+        "Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.",
+        "JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.",
+        "Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:",
+      ],
+    },
+    {
+      id: "4",
+      type: "CODE",
+      code: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;',
+    },
+    {
+      id: "5",
+      type: "TEXT",
+      title: "Заголовок этого блока",
+      paragraphs: [
+        "Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.",
+        "Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:",
+      ],
+    },
+    {
+      id: "2",
+      type: "IMAGE",
+      src: "https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png",
+      title: "Рисунок 1 - скриншот сайта",
+    },
+    {
+      id: "3",
+      type: "CODE",
+      code: "const path = require('path');\n\nconst server = jsonServer.create();\n\nconst router = jsonServer.router(path.resolve(__dirname, 'db.json'));\n\nserver.use(jsonServer.defaults({}));\nserver.use(jsonServer.bodyParser);",
+    },
+    {
+      id: "7",
+      type: "TEXT",
+      title: "Заголовок этого блока",
+      paragraphs: [
+        "JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.",
+        "Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:",
+      ],
+    },
+    {
+      id: "8",
+      type: "IMAGE",
+      src: "https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png",
+      title: "Рисунок 1 - скриншот сайта",
+    },
+    {
+      id: "9",
+      type: "TEXT",
+      title: "Заголовок этого блока",
+      paragraphs: [
+        "JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.",
+      ],
+    },
+  ],
+} as Article;
+
+const articles = Array.from({ length: 5 }).map(() => articleItem);
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const GridLight: Story = {
+  args: {
+    articles,
+  },
+};
+
+export const GridDark: Story = {
+  decorators: [ThemeDecorator(Theme.Dark)],
+  parameters: {
+    backgrounds: { default: "dark" },
+  },
+  args: {
+    articles,
+  },
+};
+
+export const ListLight: Story = {
+  args: {
+    articles,
+    view: ArticleView.LIST,
+  },
+};
+
+export const ListDark: Story = {
+  decorators: [ThemeDecorator(Theme.Dark)],
+  parameters: {
+    backgrounds: { default: "dark" },
+  },
+  args: {
+    articles,
+    view: ArticleView.LIST,
+  },
+};
