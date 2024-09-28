95001505e15e5413f5cf4fe3b83c2602a872a92e
Dmytro <dima06444@gmail.com>
2024-09-28 12:39:53 +0300

    test: add initializeArticlePage test

---
+++ src/pages/ArticlesPage/model/services/initializeArticlePage/initializeArticlePage.test.ts
@@ -0,0 +1,48 @@
+import { TestAsyncThunk } from "shared/lib/tests/TestAsyncThunk";
+import { articlePageActions } from "pages/ArticlesPage/model/slices/articlePageSlice";
+import { fetchArticleList } from "../fetchArticleList/fetchArticleList";
+import { initializeArticlePage } from "./initializeArticlePage";
+
+jest.mock("../fetchArticleList/fetchArticleList");
+jest.spyOn(articlePageActions, "initState");
+
+describe("initializeArticlePage.test", () => {
+  test("success", async () => {
+    const thunk = new TestAsyncThunk(initializeArticlePage, {
+      articlePage: {
+        page: 2,
+        ids: [],
+        entities: {},
+        limit: 5,
+        isLoading: false,
+        hasMore: true,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toHaveBeenCalledTimes(4);
+    expect(fetchArticleList).toHaveBeenCalledWith({ page: 1 });
+    expect(articlePageActions.initState).toHaveBeenCalled();
+  });
+
+  test("should fetchArticleList not call when _initialized is true", async () => {
+    const thunk = new TestAsyncThunk(initializeArticlePage, {
+      articlePage: {
+        page: 2,
+        ids: [],
+        entities: {},
+        limit: 5,
+        isLoading: false,
+        hasMore: false,
+        _initialized: true,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+    expect(fetchArticleList).not.toHaveBeenCalled();
+    expect(articlePageActions.initState).not.toHaveBeenCalled();
+  });
+});
