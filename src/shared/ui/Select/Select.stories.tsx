28c0cb1fd1a9c5acb7ac6e86942fa1c06298deda
Dmytro <dima06444@gmail.com>
2024-09-29 18:56:25 +0300

    fix: storybook error

--- src/shared/ui/Select/Select.stories.tsx
+++ src/shared/ui/Select/Select.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Select } from "shared/ui/Select/Select";
+import { MemoSelect as Select } from "shared/ui/Select/Select";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
