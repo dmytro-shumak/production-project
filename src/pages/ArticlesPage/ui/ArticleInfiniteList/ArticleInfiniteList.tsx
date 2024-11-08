83f250bc974a4313c14b783804305bdaec950c6e
Dmytro <dima06444@gmail.com>
2024-11-08 20:50:25 +0200

    fix: articles

--- src/pages/ArticlesPage/ui/ArticleInfiniteList/ArticleInfiniteList.tsx
+++ src/pages/ArticlesPage/ui/ArticleInfiniteList/ArticleInfiniteList.tsx
@@ -24,6 +24,7 @@ export const ArticleInfiniteList = memo(({ className }: Props) => {
         isLoading={isLoading}
         articles={articles}
         className={className}
+        virtualized={false} // TODO:remove this
       />
     </div>
   );
