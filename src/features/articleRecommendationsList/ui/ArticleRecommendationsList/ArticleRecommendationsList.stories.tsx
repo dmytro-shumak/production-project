c2652f4ea5bba8f2e9340adc76fd1c9c43c6cb2b
Dmytro <dima06444@gmail.com>
2024-11-13 22:10:57 +0200

    feat: add storybook addon mock

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories.tsx
@@ -1,13 +1,44 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Theme } from "app/providers/theme";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator";
+import { ArticleType, type Article } from "entities/Article";
+import { baseUrl } from "shared/api";
 import { ArticleRecommendationsList } from "./ArticleRecommendationsList";
+
+const article: Article = {
+  id: "1",
+  title: "Javascript news",
+  subtitle: "Что нового в JS за 2022 год?",
+  img: "https://teknotower.com/wp-content/uploads/2020/11/js.png",
+  views: 1022,
+  createdAt: "26.02.2022",
+  type: [ArticleType.ALL],
+  user: {
+    id: "1",
+    username: "Ulbi Tv",
+    avatar:
+      "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&q=70&fm=webp",
+  },
+  blocks: [],
+};
 
 const meta = {
   title: "features/ArticleRecommendationsList",
   component: ArticleRecommendationsList,
   parameters: {
     layout: "fullscreen",
+    mockData: [
+      {
+        url: `${baseUrl}/articles?_limit=3`,
+        method: "GET",
+        status: 200,
+        response: [
+          { ...article, id: "1" },
+          { ...article, id: "2" },
+          { ...article, id: "3" },
+        ],
+      },
+    ],
   },
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
