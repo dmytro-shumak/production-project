4de744a219efeffc3e1fbfeee294061d94ab0d5c
Dmytro <dima06444@gmail.com>
2024-11-27 20:42:20 +0200

    feat: add mobil gesture animation to drawer

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,7 +1,9 @@
-import { memo, type ReactNode } from "react";
+import { memo, useCallback, useEffect, type ReactNode } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/theme";
 import { useModal } from "shared/lib";
+import { useDrag } from "@use-gesture/react";
+import { a, useSpring, config } from "@react-spring/web";
 import { Overlay } from "../Overlay";
 import { Portal } from "../Portal/Portal";
 import styles from "./Drawer.module.css";
@@ -13,29 +15,92 @@ interface Props {
   onClose?: () => void;
 }
 
-export const Drawer = memo((props: Props) => {
-  const { className, children, onClose, isOpen } = props;
-  const { theme } = useTheme();
+const height = window.innerHeight - 100;
 
-  const { contentClick } = useModal({ isOpen, onClose });
+// TODO: improve first animation
+export const Drawer = memo(
+  ({ children, className, isOpen, onClose }: Props) => {
+    const [{ y }, api] = useSpring(() => ({ y: 0 }));
+    const { theme } = useTheme();
 
-  return (
-    <Portal>
-      <div
-        className={classNames(
-          styles.drawer,
-          {
-            [styles.opened]: isOpen,
-          },
-          [className, theme, "app_drawer"],
-        )}
-      >
-        <Overlay onClick={onClose} className={styles.overlay}>
-          <div className={styles.content} onClick={contentClick}>
+    const openDrawer = useCallback(() => {
+      api.start({ y: 0, immediate: false });
+    }, [api]);
+
+    const close = (velocity = 0) => {
+      api.start({
+        y: height,
+        immediate: false,
+        config: { ...config.stiff, velocity },
+        onResolve: onClose,
+      });
+    };
+
+    useEffect(() => {
+      if (isOpen) {
+        openDrawer();
+      }
+    }, [isOpen, openDrawer]);
+
+    const bind = useDrag(
+      ({
+        last,
+        velocity: [, vy],
+        direction: [, dy],
+        movement: [, my],
+        cancel,
+      }) => {
+        if (my < -70) cancel();
+
+        if (last) {
+          if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+            close();
+          } else {
+            openDrawer();
+          }
+        } else {
+          api.start({ y: my, immediate: true });
+        }
+      },
+      {
+        from: () => [0, y.get()],
+        filterTaps: true,
+        bounds: { top: 0 },
+        rubberband: true,
+      },
+    );
+
+    useModal({ isOpen, onClose });
+
+    const display = y.to((py) => {
+      return py <= height ? "block" : "none";
+    });
+
+    return (
+      <Portal>
+        <div
+          className={classNames(
+            styles.drawer,
+            {
+              [styles.opened]: isOpen,
+            },
+            [className, theme, "app_drawer"],
+          )}
+        >
+          <Overlay onClick={onClose} className={styles.overlay} />
+          <a.div
+            className={styles.sheet}
+            style={{
+              display,
+              bottom: `calc(-100vh + ${height - 100}px)`,
+              y,
+            }}
+            {...bind()}
+          >
             {children}
-          </div>
-        </Overlay>
-      </div>
-    </Portal>
-  );
-});
+          </a.div>
+        </div>
+      </Portal>
+    );
+  },
+);
