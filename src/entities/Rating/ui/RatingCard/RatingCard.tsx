f5a3917d3765eec67766749d5b9b37c92bb03578
Dmytro <dima06444@gmail.com>
2024-12-05 22:55:13 +0200

    feat: add rating card for mobile devices

--- src/entities/Rating/ui/RatingCard/RatingCard.tsx
+++ src/entities/Rating/ui/RatingCard/RatingCard.tsx
@@ -1,5 +1,6 @@
-import { memo, useCallback, useState } from "react";
+import { Fragment, memo, useCallback, useState, type ChangeEvent } from "react";
 import { useTranslation } from "react-i18next";
+import { BrowserView, MobileView } from "react-device-detect";
 import { classNames } from "@/shared/lib/classNames/classNames";
 import styles from "./RatingCard.module.css";
 import {
@@ -13,6 +14,7 @@ import {
 } from "@/shared/ui";
 import { Modal } from "@/shared/ui/Modal/Modal";
 import { Input } from "@/shared/ui/Input/Input";
+import { Drawer } from "@/shared/ui/Drawer";
 
 interface Props {
   className?: string;
@@ -34,10 +36,48 @@ export const RatingCard = memo(
   }: Props) => {
     const { t } = useTranslation();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [starsCount, setStarsCount] = useState(0);
+    const [feedback, setFeedback] = useState("");
 
-    const handleSelectStars = useCallback(() => {
-      setIsModalOpen(true);
-    }, []);
+    const handleSelectStars = useCallback(
+      (selectedStarsCount: number) => {
+        setStarsCount(selectedStarsCount);
+        if (hasFeedback) {
+          setIsModalOpen(true);
+        } else {
+          onAccept?.(selectedStarsCount);
+        }
+      },
+      [hasFeedback, onAccept],
+    );
+
+    const acceptHandle = useCallback(() => {
+      setIsModalOpen(false);
+      onAccept?.(starsCount, feedback);
+    }, [feedback, onAccept, starsCount]);
+
+    const cancelHandle = useCallback(() => {
+      setIsModalOpen(false);
+      onCancel?.(starsCount);
+    }, [onCancel, starsCount]);
+
+    const handleInputChange = useCallback(
+      (e: ChangeEvent<HTMLInputElement>) => {
+        setFeedback(e.target.value);
+      },
+      [],
+    );
+
+    const modalContent = (
+      <>
+        <Text title={feedbackTitle} />
+        <Input
+          value={feedback}
+          onChange={handleInputChange}
+          placeholder={t("YourFeedback")}
+        />
+      </>
+    );
 
     return (
       <Card className={classNames("", {}, [className])}>
@@ -45,16 +85,27 @@ export const RatingCard = memo(
           <Text title={title} />
           <StarRating size={40} onSelect={handleSelectStars} />
         </VStack>
-        <Modal isOpen={isModalOpen} contentClassName={styles.modalContent}>
-          <VStack gap={32}>
-            <Text title={feedbackTitle} />
-            <Input placeholder={t("YourFeedback")} />
-            <HStack gap={16} justify="end">
-              <Button theme={ButtonTheme.OutlineRed}>{t("Close")}</Button>
-              <Button>{t("Send")}</Button>
-            </HStack>
-          </VStack>
-        </Modal>
+        <BrowserView>
+          <Modal isOpen={isModalOpen} contentClassName={styles.modalContent}>
+            <VStack gap={32}>
+              {modalContent}
+              <HStack gap={16} justify="end">
+                <Button onClick={cancelHandle} theme={ButtonTheme.OutlineRed}>
+                  {t("Close")}
+                </Button>
+                <Button onClick={acceptHandle}>{t("Send")}</Button>
+              </HStack>
+            </VStack>
+          </Modal>
+        </BrowserView>
+        <MobileView>
+          <Drawer isOpen={isModalOpen} onClose={cancelHandle}>
+            <VStack gap={32}>
+              {modalContent}
+              <Button onClick={acceptHandle}>{t("Send")}</Button>
+            </VStack>
+          </Drawer>
+        </MobileView>
       </Card>
     );
   },
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,7 +2,6 @@ import { type FC } from "react";
 import { useTranslation } from "react-i18next";
 import { Counter } from "@/entities/Counter";
 import { Page } from "@/widgets/Page";
-import { RatingCard } from "@/entities/Rating";
 
 const MainPage: FC = () => {
   const { t } = useTranslation();
@@ -10,11 +9,6 @@ const MainPage: FC = () => {
   return (
     <Page>
       <h1>{t("MainPage")}</h1>
-      <RatingCard
-        title="Do you like the article?"
-        feedbackTitle="feedback title"
-        hasFeedback
-      />
       <Counter />
     </Page>
   );
