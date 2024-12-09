9796c3c2c71e7d0e0f2cb1e30ccfeb55c6e58ebf
Dmytro <dima06444@gmail.com>
2024-12-09 22:02:44 +0200

    chore: add alias to storybook configuration

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -63,6 +63,13 @@ const config: StorybookConfig = {
       );
     }
 
+    if (config.resolve?.alias) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../../src"),
+      };
+    }
+
     return config;
   },
 };
