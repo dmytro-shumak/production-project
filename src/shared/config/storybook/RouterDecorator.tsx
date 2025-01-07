24d263bf7b5870c015cb509d39c255013d463b45
Dmytro <dima06444@gmail.com>
2025-01-08 00:28:17 +0200

    fix: router decorator

--- src/shared/config/storybook/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator.tsx
@@ -1,10 +1,19 @@
 import type { StoryFn } from "@storybook/react";
 // eslint-disable-next-line production-shumak-plugin/layer-imports
 import "@/app/styles/index.css";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
-export const RouterDecorator = (StoryComponent: StoryFn) => (
-  <BrowserRouter>
-    <StoryComponent />
-  </BrowserRouter>
-);
+interface RouterDecoratorProps {
+  initialEntries?: string[];
+  path?: string;
+}
+
+export const RouterDecorator =
+  ({ path, initialEntries = ["/"] }: RouterDecoratorProps) =>
+  (StoryComponent: StoryFn) => (
+    <MemoryRouter initialEntries={initialEntries}>
+      <Routes>
+        <Route path={path} element={<StoryComponent />} />
+      </Routes>
+    </MemoryRouter>
+  );
