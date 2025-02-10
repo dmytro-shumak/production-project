0b93f56a3b1250cd4cc95e4274cc86e850a07d2d
Dmytro <dima06444@gmail.com>
2025-02-10 22:02:21 +0200

    feat: redesign ArticleViewSelector with new icons and responsive layout

--- src/features/articleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/features/articleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -3,10 +3,19 @@ import { memo } from "react";
 import styles from "./ArticleViewSelector.module.css";
 
 import { ArticleView } from "@/entities/Article";
-import GridIcon from "@/shared/assets/icons/grid.svg?react";
-import ListIcon from "@/shared/assets/icons/list.svg?react";
+import GridIcon from "@/shared/assets/icons/grid-new.svg?react";
+import GridIconDeprecated from "@/shared/assets/icons/grid.svg?react";
+import ListIcon from "@/shared/assets/icons/list-new.svg?react";
+import ListIconDeprecated from "@/shared/assets/icons/list.svg?react";
 import { classNames } from "@/shared/lib";
-import { Button, ButtonTheme, Icon } from "@/shared/ui";
+import { ToggleFeatures, toggleFeatures } from "@/shared/lib/features";
+import {
+  Button as ButtonDeprecated,
+  ButtonTheme,
+  Icon as IconDeprecated,
+} from "@/shared/ui";
+import { Card } from "@/shared/ui/redesigned/Card";
+import { Icon } from "@/shared/ui/redesigned/Icon";
 
 interface Props {
   className?: string;
@@ -17,11 +26,19 @@ interface Props {
 const viewTypes = [
   {
     view: ArticleView.GRID,
-    icon: GridIcon,
+    icon: toggleFeatures({
+      name: "isAppRedesigned",
+      on: () => GridIcon,
+      off: () => GridIconDeprecated,
+    }),
   },
   {
     view: ArticleView.LIST,
-    icon: ListIcon,
+    icon: toggleFeatures({
+      name: "isAppRedesigned",
+      on: () => ListIcon,
+      off: () => ListIconDeprecated,
+    }),
   },
 ];
 
@@ -30,22 +47,46 @@ export const ArticleViewSelector = memo(
     const onClick = (newView: ArticleView) => () => onViewClick?.(newView);
 
     return (
-      <div className={classNames("", {}, [className])}>
-        {viewTypes.map((viewType) => (
-          <Button
-            theme={ButtonTheme.Clear}
-            onClick={onClick(viewType.view)}
-            key={viewType.view}
+      <ToggleFeatures
+        featureName="isAppRedesigned"
+        on={
+          <Card
+            className={classNames(styles.articleViewSelectorRedesigned, {}, [
+              className,
+            ])}
+            borderRadius={42}
           >
-            <Icon
-              Svg={viewType.icon}
-              className={classNames("", {
-                [styles.notSelected]: view !== viewType.view,
-              })}
-            />
-          </Button>
-        ))}
-      </div>
+            {viewTypes.map((viewType) => (
+              <Icon
+                clickable
+                onClick={onClick(viewType.view)}
+                Svg={viewType.icon}
+                className={classNames("", {
+                  [styles.notSelected]: view !== viewType.view,
+                })}
+              />
+            ))}
+          </Card>
+        }
+        off={
+          <div className={classNames("", {}, [className])}>
+            {viewTypes.map((viewType) => (
+              <ButtonDeprecated
+                theme={ButtonTheme.Clear}
+                onClick={onClick(viewType.view)}
+                key={viewType.view}
+              >
+                <IconDeprecated
+                  Svg={viewType.icon}
+                  className={classNames("", {
+                    [styles.notSelected]: view !== viewType.view,
+                  })}
+                />
+              </ButtonDeprecated>
+            ))}
+          </div>
+        }
+      />
     );
   },
 );
