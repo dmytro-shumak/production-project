4d7d887e324f6eff1a53ae4e5d34eb1ba4fcb09f
Dmytro <dima06444@gmail.com>
2025-02-10 22:46:19 +0200

    refactor: enhance ListBox component with generic type support and memoization

--- src/shared/ui/redesigned/Popups/ui/ListBox/ListBox.tsx
+++ src/shared/ui/redesigned/Popups/ui/ListBox/ListBox.tsx
@@ -4,7 +4,7 @@ import {
   ListboxOption,
   ListboxOptions,
 } from "@headlessui/react";
-import { memo, type ReactNode } from "react";
+import { type ReactNode } from "react";
 
 import { HStack } from "../../../../redesigned/Stack";
 import { Button } from "../../../Button";
@@ -13,77 +13,78 @@ import popupStyles from "../../styles/popup.module.css";
 import styles from "./ListBox.module.css";
 
 import { classNames } from "@/shared/lib";
+import { typedMemo } from "@/shared/types";
 
-export interface ListBoxItem {
-  value: string;
+export interface ListBoxItem<T extends string> {
+  value: T;
   content: ReactNode;
   disabled?: boolean;
 }
 
-interface Props {
+interface Props<T extends string> {
   readOnly?: boolean;
-  items: ListBoxItem[];
-  value?: string;
+  items: ListBoxItem<T>[];
+  value?: T;
   defaultValue?: string;
   className?: string;
   label?: string;
-  onChange?: <T extends string>(value: T) => void;
+  onChange?: (value: T) => void;
 }
 
-export const ListBox = memo(
-  ({
-    className,
-    items,
-    onChange,
-    defaultValue,
-    value,
-    readOnly,
-    label,
-  }: Props) => {
-    return (
-      <HStack gap={8} justify="start">
-        {label && <span>{label}</span>}
-        <Listbox
-          as="div"
-          value={value}
-          disabled={readOnly}
-          onChange={onChange}
-          className={classNames(styles.listBox, {}, [
-            className,
-            popupStyles.menu,
+const ListBox = <T extends string>({
+  className,
+  items,
+  onChange,
+  defaultValue,
+  value,
+  readOnly,
+  label,
+}: Props<T>) => {
+  return (
+    <HStack gap={8} justify="start">
+      {label && <span>{label}</span>}
+      <Listbox
+        as="div"
+        value={value}
+        disabled={readOnly}
+        onChange={onChange}
+        className={classNames(styles.listBox, {}, [
+          className,
+          popupStyles.menu,
+        ])}
+      >
+        <ListboxButton disabled={readOnly} as="div">
+          <Button disabled={readOnly}>{value ?? defaultValue}</Button>
+        </ListboxButton>
+        <ListboxOptions
+          anchor="bottom"
+          className={classNames(styles.options, {}, [
+            "w-[var(--button-width)]",
           ])}
         >
-          <ListboxButton disabled={readOnly} as="div">
-            <Button disabled={readOnly}>{value ?? defaultValue}</Button>
-          </ListboxButton>
-          <ListboxOptions
-            anchor="bottom"
-            className={classNames(styles.options, {}, [
-              "w-[var(--button-width)]",
-            ])}
-          >
-            {items.map((item) => (
-              <ListboxOption
-                key={item.value}
-                value={item.value}
-                disabled={item.disabled}
-              >
-                {({ focus, selected, disabled }) => (
-                  <div
-                    className={classNames(styles.item, {
-                      [styles.active]: focus,
-                      [styles.disabled]: disabled,
-                    })}
-                  >
-                    {selected && "✓"}
-                    {item.content}
-                  </div>
-                )}
-              </ListboxOption>
-            ))}
-          </ListboxOptions>
-        </Listbox>
-      </HStack>
-    );
-  },
-);
+          {items.map((item) => (
+            <ListboxOption
+              key={item.value}
+              value={item.value}
+              disabled={item.disabled}
+            >
+              {({ focus, selected, disabled }) => (
+                <div
+                  className={classNames(styles.item, {
+                    [styles.active]: focus,
+                    [styles.disabled]: disabled,
+                  })}
+                >
+                  {selected && "✓"}
+                  {item.content}
+                </div>
+              )}
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </Listbox>
+    </HStack>
+  );
+};
+
+export const MemoListBox = typedMemo(ListBox);
