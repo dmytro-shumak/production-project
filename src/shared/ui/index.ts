7f9f3c6b77a56d53df1247121de53fe78fb20ce1
Dmytro <dima06444@gmail.com>
2025-02-20 20:45:08 +0200

    refactor: remove deprecated Button component and associated styles, tests, and files

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,4 @@
-export * from "./deprecated/Button";
 export * from "./redesigned/Stack";
-export { Button } from "./deprecated/Button/Button";
 export { Code } from "./redesigned/Code/Code";
 
 export * from "./redesigned/StarRating";
